b7c161d9f87b5a1bd1e7062701f497c3ed4c7168
rivalda11 <rivaldasyahril@gmail.com>
2025-10-02 01:59:03 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-pink-50 flex flex-col items-center justify-center p-6">
+      <h1 className="text-5xl font-extrabold text-pink-600 mb-4">
+        Undangan Pernikahan
+      </h1>
+      <p className="text-lg text-pink-700">
+        Kami mengundang Anda untuk hadir di acara pernikahan kami.
+      </p>
+    </div>
+  );
+}
